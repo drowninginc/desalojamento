931fd338be9289846cf28bb29a5ad94a2b5a5434
Cláudio Lemos <claudio.lemos@tensorops.ai>
2024-02-24 23:09:52 +0000

    new stuff

--- web/components/linechart.tsx
+++ web/components/linechart.tsx
@@ -1,145 +1,134 @@
 import React, { useEffect, useRef } from 'react'
 import * as d3 from 'd3'
+import { json } from 'd3-fetch'
+
+type Data = {
+  Habitacao: Record<string, number>
+  AL: Record<string, number>
+}
 
 type Props = {
   language: string
 }
 
 const Linechart = ({ language }: Props) => {
-  const svg = useRef(null)
+  const svgRef = useRef<SVGSVGElement>(null)
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('/static/data/habitacao.json');
-      const data = await response.json();
+      const response = await json<Data>('/static/data/habitacao.json')
 
-      const margin = { top: 20, right: 30, bottom: 40, left: 50 };
-      const width = 460 - margin.left - margin.right;
-      const height = 400 - margin.top - margin.bottom;
+      if (!response) {
+        return
+      }
 
-      const svgElement = d3.select(svg.current)
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", `translate(${margin.left},${margin.top})`);
+      const margin = { top: 20, right: 30, bottom: 40, left: 50 }
+      const width = 460 - margin.left - margin.right
+      const height = 400 - margin.top - margin.bottom
 
-      const xScale = d3.scaleLinear()
-        .domain(d3.extent(Object.keys(data.Habitacao)))
-        .range([0, width]);
-      const yScale = d3.scaleLinear()
-        .domain([0, d3.max([...Object.values(data.Habitacao), ...Object.values(data.AL)])])
-        .range([height, 0]);
+      const svgElement = d3
+        .select(svgRef.current)
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`)
+
+      const xScale = d3
+        .scaleLinear()
+        .domain(d3.extent(Object.keys(response.Habitacao)) as [number, number])
+        .range([0, width])
+      const yScale = d3
+        .scaleLinear()
+        .domain([
+          0,
+          d3.max([...Object.values(response.Habitacao), ...Object.values(response.AL)]) as number,
+        ])
+        .range([height, 0])
 
       // Define the lines
-      const lineHabitacao = d3.line()
-        .x((d, i) => xScale(Object.keys(data.Habitacao)[i]))
-        .y((d) => yScale(d));
-      const lineAL = d3.line()
-        .x((d, i) => xScale(Object.keys(data.AL)[i]))
-        .y((d) => yScale(d));
+      const lineHabitacao = d3
+        .line<number>()
+        .x((d, i) => xScale(Number(Object.keys(response.Habitacao)[i])))
+        .y(d => yScale(d))
+      const lineAL = d3
+        .line<number>()
+        .x((d, i) => xScale(Number(Object.keys(response.AL)[i])))
+        .y(d => yScale(d))
 
       // Draw the lines
-      const pathHab = svgElement.append("path")
-        .datum(Object.values(data.Habitacao))
-        .attr("class", "graph-line")
-        .attr("class", "line-habitacao")
-        .attr("fill", "none")
-        .attr("d", lineHabitacao);
+      const pathHab = svgElement
+        .append('path')
+        .datum(Object.values(response.Habitacao))
+        .attr('class', 'graph-line line-habitacao')
+        .attr('fill', 'none')
+        .attr('d', lineHabitacao)
 
-      // const pathAL = svgElement.append("path")
-      //   .datum(Object.values(data.AL))
-      //   .attr("class", "graph-line")
-      //   .attr("class", "line-al")
-      //   .attr("fill", "none")
-      //   .attr("stroke", "none")
-      //   .attr("d", lineAL);
+      const pathLength = pathHab.node()!.getTotalLength()
 
-      const pathLength = pathHab.node().getTotalLength();
+      const transitionPath = d3.transition().ease(d3.easeSin).duration(5000)
 
-      const transitionPath = d3
-        .transition()
-        .ease(d3.easeSin)
-        .duration(5000);
-
-        pathHab
-        .attr("stroke-dashoffset", pathLength)
-        .attr("stroke-dasharray", pathLength)
+      pathHab
+        .attr('stroke-dashoffset', pathLength)
+        .attr('stroke-dasharray', pathLength)
         .transition(transitionPath)
-        .attr("stroke-dashoffset", 0);
+        .attr('stroke-dashoffset', 0)
 
+      const radius = 8
 
-  const radius = 8;
-  function repeatTransitionHabitacao(selection) {
-    selection
-      .transition()
-      .delay(10000)
-      .attr("r", 0) // Reset to radius 0
-      .on("end", () => repeatTransitionHabitacao(selection)); // Repeat the transition
-  }
+      svgElement
+        .selectAll('.circle-habitacao')
+        .data(Object.entries(response.Habitacao))
+        .enter()
+        .append('circle')
+        .attr('class', 'graph-circle circle-habitacao')
+        .attr('cx', d => xScale(Number(d[0])))
+        .attr('cy', d => yScale(d[1]))
+        .attr('r', radius)
 
-  const circlesHabitacao = svgElement.selectAll(".circle-habitacao")
-    .data(Object.entries(data.Habitacao))
-    .enter().append("circle")
-      .attr("class", "graph-circle")
-      .attr("class", "circle-habitacao")
-      .attr("cx", d => xScale(d[0]))
-      .attr("cy", d => yScale(d[1]))
-      .attr("r", 0)
-      .transition() // Start a transition
-      .delay((d, i) => i * 1000) // Delay each circle's animation based on its index
-      .attr("r", radius);
+      svgElement
+        .selectAll('.circle-al')
+        .data(Object.entries(response.AL).slice(4))
+        .enter()
+        .append('circle')
+        .attr('class', 'graph-circle circle-al')
+        .attr('cx', d => xScale(Number(d[0])))
+        .attr('cy', d => yScale(d[1]))
+        .attr('r', radius)
 
+      // Add a label with the value
+      svgElement
+        .selectAll('.label-habitacao')
+        .data(Object.entries(response.Habitacao))
+        .enter()
+        .append('text')
+        .attr('class', 'graph-label label-habitacao')
+        .attr('x', d => xScale(Number(d[0])))
+        .attr('y', d => yScale(d[1]) - 10)
+        .text(d => d[1].toString())
 
+      svgElement
+        .selectAll('.label-al')
+        .data(Object.entries(response.AL))
+        .enter()
+        .append('text')
+        .attr('class', 'graph-label label-al')
+        .attr('x', d => xScale(Number(d[0])))
+        .attr('y', d => yScale(d[1]) - 10)
+        .text(d => d[1].toString())
 
-  const circlesAL = svgElement.selectAll(".circle-al")
-    .data(Object.entries(data.AL).slice(4))
-    .enter().append("circle")
-      .attr("class", "graph-circle")
-      .attr("class", "circle-al")
-      .attr("cx", d => xScale(d[0]))
-      .attr("cy", d => yScale(d[1]))
-      .attr("r", 0)
-      .transition() // Start a transition
-      .delay((d, i) => i * 5000) // Delay each circle's animation based on its index
-      .attr("r", radius);
+      svgElement
+        .append('g')
+        .attr('transform', `translate(0,${height - margin.bottom})`)
+        .call(d3.axisBottom(xScale))
+    }
 
-  // Add a label with the value
-  svgElement.selectAll(".label-habitacao")
-    .data(Object.entries(data.Habitacao))
-    .enter().append("text")
-      .attr("class", "graph-label")
-      .attr("class", "label-habitacao")
-      .attr("x", d => xScale(d[0]))
-      .attr("y", d => yScale(d[1]) - 10)
-      .text(d => d[1])
-
-  svgElement.selectAll(".label-al")
-    .data(Object.entries(data.AL))
-    .enter().append("text")
-      .attr("class", "graph-label")
-      .attr("class", "label-al")
-      .attr("x", d => xScale(d[0]))
-      .attr("y", d => yScale(d[1]) - 10)
-      .text(d => d[1])
-
-      svgElement.append("g")
-      .attr("transform", `translate(0,${height - margin.bottom})`)
-      .call(d3.axisBottom(xScale));
-    };
-
-    fetchData();
-  }, [language]);
-
-
-
-
-
-
+    fetchData()
+  }, [language])
 
   return (
     <div className="histogram-container">
       <h1 className="histogram-title">Habitação e ALs no Porto</h1>
-      <svg ref={svg} className="histogram-svg"></svg>
+      <svg ref={svgRef} className="histogram-svg"></svg>
     </div>
   )
 }
