9aa486262a5af8f170e2b9e1aab6d557caadb7c4
João Narciso <joaobernardonarciso@gmail.com>
2025-07-27 00:10:39 +0100

    scroll fix

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,9 +1,23 @@
 import { AppProps } from 'next/app'
+import { useEffect } from 'react'
 // eslint-disable-next-line import/no-unresolved
 import { Analytics } from '@vercel/analytics/react'
 import '../sass/_main.scss'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      window.scrollTo(0, 0)
+    }
+
+    window.onbeforeunload = handleBeforeUnload
+
+    // Cleanup function to remove the event listener
+    return () => {
+      window.onbeforeunload = null
+    }
+  }, [])
+
   return (
     <>
       <Component {...pageProps} />
