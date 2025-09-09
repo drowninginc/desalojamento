451a05bfc4f58f3322470f69c08af8e84a5ec2b4
João Narciso <joaobernardonarciso@gmail.com>
2025-09-09 15:51:04 +0100

    remove language selector

--- web/components/extras/triggers.ts
+++ web/components/extras/triggers.ts
@@ -427,7 +427,7 @@ export const createScrollTriggers = (
       setTriggerMegaHostAnimation(true)
       setLayerVisibility(city, map.current, `${city}-al-megahosts`)
       gsap.to('.footer', { opacity: 1, duration: 0.5 })
-      gsap.to('.language-selector', { opacity: 0, duration: 0.5 })
+      gsap.to('.language-selector', { display: 'none', duration: 0.5 })
     },
     onEnterBack: () => {
       setLayerVisibility(city, map.current, `${city}-al-megahosts`)
@@ -440,7 +440,7 @@ export const createScrollTriggers = (
 
     onLeaveBack: () => {
       gsap.to('.footer', { opacity: 0, duration: 0.5 })
-      gsap.to('.language-selector', { opacity: 1, duration: 0.5 })
+      gsap.to('.language-selector', { display: 'block', duration: 0.5 })
     },
   })
 
