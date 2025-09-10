556e04ba628f501b736e45c273b2ad99049ef66f
João Narciso <joaobernardonarciso@gmail.com>
2025-09-10 14:53:04 +0100

    mais meta

--- web/components/meta.tsx
+++ web/components/meta.tsx
@@ -10,12 +10,29 @@ const Meta = () => {
       <meta name="theme-color" content="#eee5e9" />
       <meta name="desALojamento" content="desALojamento" />
       <title>desALojamento</title>
+
+      {/* Open Graph meta tags for social sharing */}
       <meta property="og:title" content="desALojamento" />
       <meta
         property="og:description"
         content="Alguns dados e mapas sobre o Alojamento Local no Porto e em Lisboa"
       />
       <meta property="og:image" content="/thumb.jpg" />
+      <meta property="og:image:width" content="2880" />
+      <meta property="og:image:height" content="1436" />
+      <meta property="og:image:alt" content="desALojamento - Dados sobre Alojamento Local" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="desALojamento" />
+      <meta property="og:url" content="https://desalojamento.pt" />
+
+      {/* Twitter Card meta tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="desALojamento" />
+      <meta
+        name="twitter:description"
+        content="Alguns dados e mapas sobre o Alojamento Local no Porto e em Lisboa"
+      />
+      <meta name="twitter:image" content="/thumb.jpg" />
     </Head>
   )
 }
