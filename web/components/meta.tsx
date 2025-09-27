1ff1c5e405664d0fa0fb1aaba58e842fb7cba239
João Narciso <joaobernardonarciso@gmail.com>
2025-09-27 01:59:44 +0100

    meta

--- web/components/meta.tsx
+++ web/components/meta.tsx
@@ -17,22 +17,26 @@ const Meta = () => {
         property="og:description"
         content="Alguns dados e mapas sobre o Alojamento Local em Lisboa e no Porto"
       />
-      <meta property="og:image" content="/thumb.jpg" />
+      <meta property="og:image" content="https://www.desalojamento.pt/thumb.jpg" />
       <meta property="og:image:width" content="2880" />
       <meta property="og:image:height" content="1436" />
       <meta property="og:image:alt" content="desALojamento - Dados sobre Alojamento Local" />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content="desALojamento" />
-      <meta property="og:url" content="https://desalojamento.pt" />
+      <meta property="og:url" content="https://www.desalojamento.pt" />
 
       {/* Twitter Card meta tags */}
       <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@joaobernarciso" />
+      <meta name="twitter:creator" content="@joaobernarciso" />
       <meta name="twitter:title" content="desALojamento" />
       <meta
         name="twitter:description"
         content="Alguns dados e mapas sobre o Alojamento Local em Lisboa e no Porto"
       />
-      <meta name="twitter:image" content="/thumb.jpg" />
+      <meta name="twitter:image" content="https://www.desalojamento.pt/thumb.jpg" />
+      <meta name="twitter:image:alt" content="desALojamento - Dados sobre Alojamento Local" />
+      <meta name="twitter:domain" content="www.desalojamento.pt" />
     </Head>
   )
 }
